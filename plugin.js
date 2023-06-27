a792adf14a6960a1cbd73551d88b55c8e9c99b47
realmaxy <max_sem777@mail.ru>
2023-06-27 18:13:52 +0300

    wrote first plugin

--- plugin.js
+++ plugin.js
@@ -1,2 +1,20 @@
 "use strict";
-figma.showUI(__html__);
+function runPlugin() {
+    const selectedElements = figma.currentPage.selection.length;
+    if (selectedElements === 0) {
+        figma.closePlugin('Ни один элемент не выбран');
+        return;
+    }
+    if (selectedElements > 1) {
+        figma.closePlugin("Выберите один элеменет");
+        return;
+    }
+    const selectedName = figma.currentPage.selection[0].name;
+    function hasSameName(node) {
+        return node.name === selectedName;
+    }
+    const withSameName = figma.currentPage.findAll(hasSameName);
+    figma.currentPage.selection = withSameName;
+    figma.closePlugin();
+}
+runPlugin();
--- plugin.ts
+++ plugin.ts
@@ -1,1 +1,26 @@
-figma.showUI(__html__)
+function runPlugin(): void{
+  const selectedElements = figma.currentPage.selection.length
+
+  if(selectedElements === 0) {
+    figma.closePlugin('Ни один элемент не выбран')
+    return
+  }
+
+  if(selectedElements > 1) {
+    figma.closePlugin("Выберите один элеменет") 
+    return
+  }
+
+  const selectedName: string = figma.currentPage.selection[0].name
+
+  function hasSameName(node:any):boolean {
+    return node.name === selectedName
+  }
+
+  const withSameName = figma.currentPage.findAll(hasSameName)
+  figma.currentPage.selection = withSameName
+
+  figma.closePlugin()
+}
+
+runPlugin()
